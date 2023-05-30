54814cca94077920d5447acd2423d112dc32fcce
Prelipcian Tiberiu <64843705+tiberiuprelipcian16@users.noreply.github.com>
2023-05-30 15:36:07 +0300

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ import LogoHover from "../assets/logoHover.svg";
 
 function changeLogoOver() {
   let e = document.getElementById('logo');
-  e.src = {LogoHover};
+  e.src = "{LogoHover}";
 }
 
 function changeLogoDown(){
   let e = document.getElementById('logo');
-  e.src = {Logo};
+  e.src = "{Logo}";
 }
 const Header = () => {
   return (
